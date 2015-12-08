14dfa381f153cfacb1e9397f96e2cf006b2a36bd
pilauPro <leiningem001@hawaii.rr.com>
2015-12-07 17:24:54 -1000

    consolidated to one file

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -13,6 +13,18 @@ def bubble_sort(arr)
     arr
 end
 
-input = [5,4,20,1,60,100,3,7,1000,2]
-
-puts bubble_sort(input).inspect
+#assumes block like: {|x,y| x<=>y}
+def bubble_sort_by(arr, &block)
+        (arr.size - 1).times{
+            i = 0
+            while i < arr.size - 1
+                if block.call(arr[i],arr[i + 1]) == 1
+                    temp = arr[i]
+                    arr[i] = arr[i+1]
+                    arr[i+1] = temp
+                end
+            i+=1
+            end
+        }
+        arr
+end
