57f240bea6e1a881d50dc25c52114d12afed8a2c
pilauPro <leiningem001@hawaii.rr.com>
2015-12-07 16:49:25 -1000

    bubble sort, two ways

---
+++ bubble_sort/bubble_sort_by.rb
@@ -0,0 +1,19 @@
+def bubble_sort_by(arr, &block)
+        (arr.size - 1).times{
+            i = 0
+            while i < arr.size - 1
+                if block.call(arr[i],arr[i + 1]) == 1
+                    temp = arr[i]
+                    arr[i] = arr[i+1]
+                    arr[i+1] = temp
+                end
+            i+=1
+            end
+        }
+        arr
+end
+compare = Proc.new{|x,y| x<=>y}
+
+input = [5,4,20,1,60,100,3,7,1000,2]
+
+puts bubble_sort_by(input, &compare).inspect
