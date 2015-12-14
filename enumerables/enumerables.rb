db43fbfa8e57268ffee44a8cedcf26124c8f5751
pilauPro <leiningem001@hawaii.rr.com>
2015-12-14 09:39:32 -1000

    Added my_count

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -39,16 +39,26 @@ module Enumberable
 			true
 		end
 	end
+	def my_count(arr)
+    		counter = 0
+		if block_given?
+        		my_each(arr){|x| counter += 1 if yield(x)}
+    		else
+        		arr.each{|x| counter += 1 if x} 
+    		end
+    		counter
+	end
 end
 
 include Enumberable
 
-# arr = [1,2,3,4,5,6]
-arr = [false,false,nil]
+arr = [1,2,3,4,5,6]
+# arr = [false,false,nil]
 
 # my_each(arr){|x| puts "element: #{x}"}
 # my_each_with_index(arr){|item,index| puts "item: #{item} index: #{index}"}
 # arr2 = my_select(arr){|x| x%2 == 0}
 # puts arr2
 # puts my_all?(arr){|x| x.class == Fixnum}
-puts my_none?(arr)
+# puts my_none?(arr)
+# puts my_count(arr){|x| x > 10 && x < 70}
