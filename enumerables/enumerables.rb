25a812b537b309ff60ba6e66e4f667b700d68cb8
pilauPro <leiningem001@hawaii.rr.com>
2015-12-17 22:37:53 -1000

    reformatted, again

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -1,34 +1,34 @@
 module Enumberable
 	def my_each(arr)
-	    	i = 0
-	    	if block_given?
-	        	(arr.size).times{
-	            		yield arr[i]
-	            		i+= 1
-	        	}
-	        	arr
-	    	end
+    	i = 0
+    	if block_given?
+        	(arr.size).times{
+            		yield arr[i]
+            		i+= 1
+        	}
+        	arr
+    	end
 	end
 	def my_each_with_index(arr)
-	    	i = 0
-	    	(arr.size).times{
-	        	yield arr[i],i
-	        	i+= 1
-	    	}
-	    	arr
+    	i = 0
+    	(arr.size).times{
+        	yield arr[i],i
+        	i+= 1
+    	}
+    	arr
 	end
 	def my_select(arr)
 		result = []
-            	my_each(arr){|x| result << x if yield(x)}
-            	result
+		my_each(arr){|x| result << x if yield(x)}
+        result
 	end
 	def my_all?(arr)
-	    	my_each(arr){|x| return false if !yield(x)}
-	    	true
+	    my_each(arr){|x| return false if !yield(x)}
+    	true
 	end
 	def my_any?(arr)
-	    	my_each(arr){|x| return true if yield(x)}
-	    	false
+    	my_each(arr){|x| return true if yield(x)}
+    	false
 	end
 	def my_none?(arr)
 		if block_given?
@@ -42,35 +42,35 @@ module Enumberable
 	def my_count(arr)
     		counter = 0
 		if block_given?
-        		my_each(arr){|x| counter += 1 if yield(x)}
-    		else
-        		my_each(arr){|x| counter += 1 if x} 
-    		end
-    		counter
+    		my_each(arr){|x| counter += 1 if yield(x)}
+		else
+    		my_each(arr){|x| counter += 1 if x} 
+		end
+    	counter
 	end
 	def my_map(arr)
-    		new_arr = []
-    		if block_given?
-		        my_each(arr){|x| new_arr << yield(x)}
-		        new_arr
-	    	end
+		new_arr = []
+		if block_given?
+	        my_each(arr){|x| new_arr << yield(x)}
+	        new_arr
+    	end
 	end
 	def my_map_proc(arr, proc=nil)
-    		new_arr = []
-    		if block_given? && proc
-    			my_each(arr){|x| new_arr << yield(x)}
-		        my_each(arr){|x| new_arr << proc.call(x)}
-		        new_arr
+		new_arr = []
+		if block_given? && proc
+			my_each(arr){|x| new_arr << yield(x)}
+	        my_each(arr){|x| new_arr << proc.call(x)}
+	        new_arr
 		end
 	end
 	def my_inject(arr, memo=0)
-    		if block_given?
-        		my_each(arr){|x| memo = yield(memo, x)}
-        		memo
-    		end
+		if block_given?
+    		my_each(arr){|x| memo = yield(memo, x)}
+    		memo
+		end
 	end
 	def multiply_els(arr)
-    		my_inject(arr,1){|sum,x| sum = sum * x}
+		my_inject(arr,1){|sum,x| sum = sum * x}
 	end
 end
 
