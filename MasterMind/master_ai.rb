37f62e4b3c8f873d4544bd29154fb878782a9abf
Matt Leininger <leiningem001@hawaii.rr.com>
2016-02-23 13:17:11 -1000

    code breaker game type functioning, code master needs further integration.

--- MasterMind/master_ai.rb
+++ MasterMind/master_ai.rb
@@ -1,6 +1,6 @@
 class MasterMind
     
-    attr_reader :guessgrid, :exactgrid, :colorgrid, :counter, :code, :gametype
+    attr_reader :guessgrid, :exactgrid, :colorgrid, :counter, :code, :gametype, :game
 
     @@guessestosolve = []
     @@gameswon = 0
@@ -50,9 +50,93 @@ class MasterMind
         def prompt_game_type
             puts "Human, enter 1 to be the code master, or 2 to be the code breaker"
         end
-
     end
     
+   # This class contains the attributes and functions for the game
+    class Game
+        attr_accessor :code, :word_code, :guess, :num_guess, :remaining_guesses
+        
+        include GameHelper
+        
+        # creates a new code to solve and converts the code to colors, stored in a different attribute
+        def initialize
+            @code = generate_code
+            @word_code = convert_code
+            @guess = []
+            @num_guess = []
+            @remaining_guesses = 12
+        end
+        
+        # calls on translate_num_to_color to convert numbers to colors
+        def convert_code
+            @code.map{ |num| translate_num_to_color(num) }
+        end
+        
+        # takes user input and breaks it into a code guess array
+        def capture_guess(user_input)
+            @guess = user_input.scan(/./)
+        end
+
+        def verify_guess
+            verify_letters && verify_size
+        end
+
+        def verify_letters
+            @guess.all?{ |letter| letter =~ /[bwrg]/ }
+        end
+
+        def verify_size
+            @guess.size == 4
+        end
+
+        def decrement_guesses
+            @remaining_guesses -= 1
+        end
+        
+        def convert_guess
+            @num_guess = @guess.map{ |letter| translate_color_to_num(letter) }
+        end
+        
+        # returns true if the code is guessed correctly
+        def code_cracked?
+            @num_guess == @code
+        end
+        
+        # print the player's turn result
+        def turn_result
+            puts "matches: #{exact_matches}"
+            puts "color matches: #{color_matches(get_code_remainder, get_guess_remainder)}"
+        end
+        
+        # returns number of color matches for the guess. Takes unmatched code & guess arrays as inputs.
+        def color_matches(code, guess)
+            colormatches = 0
+            guess.each{ |num| colormatches += 1 if code.any?{|x| x == num} }
+            colormatches
+        end
+        
+        # makes an array of code elements not matched by the guess
+        def get_code_remainder
+            code_remainder = []
+            4.times{ |x| code_remainder << @code[x] if @num_guess[x] != @code[x] }
+            code_remainder
+        end
+        
+        # makes an array of guess elements not matching the code
+        def get_guess_remainder
+            guess_remainder = []
+            4.times{ |x| guess_remainder << @num_guess[x] if @num_guess[x] != @code[x] }
+            guess_remainder
+        end
+        
+        # returns the number of exact matches in the guess
+        def exact_matches
+            matches = 0
+            4.times{ |x| matches += 1 if @num_guess[x] == @code[x] }
+            matches
+        end
+    end
+
     include GameHelper
 
     def initialize
@@ -63,6 +147,8 @@ class MasterMind
         @counter = 0
         @bestguess = 1
         @bestcombined = 0
+        @game = Game.new
+
     end
 
 
@@ -266,6 +352,8 @@ class MasterMind
         # exact_matches == 4
         @guessgrid.last == @code
     end
+
+
 end
 
 # ****************************************************************
@@ -284,6 +372,68 @@ rescue => e
     puts e
     retry
 end
+
+
+if current.gametype == :breaker
+	# Run game as code breaker
+	# loop until code is guessed or remaining guesses run out
+	until current.game.code_cracked? || current.game.remaining_guesses == 0
+
+	    puts "The code is: #{current.game.code}"
+	    puts "Enter your code guess:"
+
+	    # Loop until valid user entry is captured
+	    begin
+
+	        # capture user code guess
+	        current.game.capture_guess(gets.chomp.downcase)
+	        
+	        # raise exception if invalid user entry
+	        raise current.game.invalid_selection unless current.game.verify_guess
+	        
+	    rescue => e
+	        puts e
+	        retry
+	    end
+	    
+	    # convert user guess to numerical
+	    current.game.convert_guess
+	    
+	    # decrement remaining guesses
+	    current.game.decrement_guesses
+
+
+	    if current.game.code_cracked?
+	        puts "You cracked the code, MasterMind!"
+	    else
+	        puts "Incorrect guess.  Your result:"
+	        puts "#{current.game.turn_result}"
+	    end
+	end
+
+else
+	# Run game as code master
+	10.times{
+	    current = MasterMind.new
+
+	    current.random_guess
+	    
+	    until current.code_cracked?
+	        current.next_guess
+	    end
+	    if current.code_cracked? 
+	            # puts "MasterMind!, in #{current.counter} guesses."
+	            MasterMind.increment_guessestosolve(current.counter)
+	            MasterMind.increment_games_won
+	    else
+	            puts "Code not cracked: #{current.code_cracked?}"
+	    end
+	}
+	puts "average guesses to break code: #{MasterMind.avg_to_solve}"
+	puts "games won: #{MasterMind.games_won}"
+	puts "winning percentage: #{MasterMind.games_won_percentage}"
+end
+
 
 
 # 10.times{
